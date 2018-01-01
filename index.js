5f7ef1c102c61d81738634e7dce37b1b9eaf6a36
Evan Harmon <evan.p.harmon@gmail.com>
2018-01-01 12:11:48 -0700

    feat(logs): added console logging

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const Hapi = require('hapi');
+const { createLogger, transports } = require('winston');
 const { graphqlHapi } = require('apollo-server-hapi');
 const { makeExecutableSchema } = require('graphql-tools');
 const note = require('./lib/note');
 const resolvers = require('./lib/resolvers');
 
-const server = new Hapi.Server({ port: 3000, host: 'localhost' });
+const logger = createLogger({
+  level: 'info',
+  transports: [new transports.Console()],
+});
+
+const server = new Hapi.Server({
+  port: 3000,
+  host: 'localhost',
+  routes: { log: { collect: true } },
+});
 
 const schema = makeExecutableSchema({ typeDefs: [note], resolvers });
 
@@ -21,12 +31,15 @@ async function StartServer() {
       },
     },
   });
+
   try {
     await server.start();
   } catch (err) {
     console.log(`Error while starting server: ${err.message}`);
   }
   console.log(`Server running at: ${server.info.uri}`);
+
+  server.events.on('response', res => logger.info(res.payload));
 }
 
 StartServer();
